c5eb63c72042940cfd3a8d408aaf1ad6b84e9257
dangtiendat24 <148854633+dangtiendat24@users.noreply.github.com>
2024-05-30 18:10:00 +0700

    Add files via upload

---
+++ MaxOfArray/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace MaxOfArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("vui lòng nhập kích thước của mảng: ");
+            int size = int.Parse(Console.ReadLine());
+            int[] array = new int[size];
+            Console.WriteLine("vui lòng nhập các phần tử của mảng: ");
+            for(int i = 0; i< size; i++)
+            {
+                Console.WriteLine($"phần tử thứ {i + 1}: ");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            int max = array[0];
+            for(int i = 0; i< size; i++)
+            {
+                if(array[i] > max)
+                {
+                    max = array[i];
+                }
+            }
+            Console.WriteLine("phần tử lớn nhất của mảng là: " + max);
+        }
+    }
+}
